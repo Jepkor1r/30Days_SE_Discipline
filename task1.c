5a30caa9b7c7be242a75c3b2f67a21c0ee411e04
Jepkor1r <jlagat007.gmail.com>
2024-01-16 00:21:16 +0300

    corrected typos

--- task1.c
+++ task1.c
@@ -9,27 +9,27 @@ int main() {
     printf("2. Back end programming languages\n");
     printf("3. Logout\n");
 	    
-    // Getting user input for choice
+    // Getting user input for option
     printf("Enter your option: ");
     scanf("%d", &option);
 
-    // Using switch case to handle different choices
+    // Using switch case to handle different options
     switch(option) {
         case 1:
             printf("HTML\nCSS\nJS\n");
-            // Additional code for viewing profile
+            // front end programming languages
             break;
         case 2:
             printf("Python\nSQL\nPHP\n");
-            // Additional code for editing information
+            // back end programming languages
             break;
         case 3:
             printf("Logging out...\n");
-            // Additional code for logging out
+            //logging out
             break;
         default:
             printf("Invalid choice!\n");
-            // Additional code for handling invalid choices
+            // handling invalid choices
 
     return 0;
 }
